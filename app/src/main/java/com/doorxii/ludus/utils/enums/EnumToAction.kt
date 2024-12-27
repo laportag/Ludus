ed5b8ca99a107bfcf052263c1187fd03c9f12245
laportag <97185441+laportag@users.noreply.github.com>
2024-12-27 02:12:41 +0000

    update enum to actions

--- app/src/main/java/com/doorxii/ludus/utils/enums/EnumToAction.kt
+++ app/src/main/java/com/doorxii/ludus/utils/enums/EnumToAction.kt
@@ -3,6 +3,7 @@ package com.doorxii.ludus.utils.enums
 import com.doorxii.ludus.actions.combatactions.BasicAttack
 import com.doorxii.ludus.actions.combatactions.CombatAction
 import com.doorxii.ludus.actions.combatactions.CombatActions
+import com.doorxii.ludus.actions.combatactions.Missio
 import com.doorxii.ludus.actions.combatactions.TiredAttack
 import com.doorxii.ludus.actions.combatactions.Wait
 
@@ -12,6 +13,7 @@ object EnumToAction {
             CombatActions.BASIC_ATTACK -> BasicAttack()
             CombatActions.TIRED_ATTACK -> TiredAttack()
             CombatActions.WAIT -> Wait()
+            CombatActions.MISSIO -> Missio()
         }
     }
 
@@ -20,6 +22,7 @@ object EnumToAction {
             is BasicAttack -> CombatActions.BASIC_ATTACK
             is TiredAttack -> CombatActions.TIRED_ATTACK
             is Wait -> CombatActions.WAIT
+            is Missio -> CombatActions.MISSIO
             else -> {CombatActions.WAIT}
         }
     }
