862f747d1dd6c7396b1acfaa86633cc4fc71d560
laportag <97185441+laportag@users.noreply.github.com>
2025-02-11 19:54:25 +0000

    refactored new/load ludus dialogues

--- app/src/main/java/com/doorxii/ludus/ui/activities/StartActivity.kt
+++ app/src/main/java/com/doorxii/ludus/ui/activities/StartActivity.kt
@@ -6,43 +6,26 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.doorxii.ludus.data.db.AppDatabase
+import com.doorxii.ludus.ui.components.LoadLudusDialogue
+import com.doorxii.ludus.ui.components.NewLudusDialogue
 import com.doorxii.ludus.ui.theme.LudusTheme
-import com.doorxii.ludus.utils.DatabaseManagement.createDb
-import com.doorxii.ludus.utils.DatabaseManagement.deleteDb
 import com.doorxii.ludus.utils.DatabaseManagement.getAllDatabases
-import com.doorxii.ludus.utils.DatabaseManagement.returnDb
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class StartActivity : ComponentActivity() {
@@ -64,7 +47,6 @@ class StartActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         setContent {
-
             LudusTheme {
                 StartMenu()
             }
@@ -73,7 +55,7 @@ class StartActivity : ComponentActivity() {
         Log.d(TAG, "dbs: ${getAllDatabases(applicationContext)}")
     }
 
-    private fun observeFlows(){
+    private fun observeFlows() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.databases.collect {
@@ -111,134 +93,26 @@ class StartActivity : ComponentActivity() {
 
         }
         if (newDialogueState.value) {
-            NewLudusDialogue {
-                newDialogueState.value = it
-            }
+            NewLudusDialogue(
+                databases = databases.value,
+                visibleCallback = { newDialogueState.value = it },
+                launchLudusManagement = { ludus, db ->
+                    launchLudusManagement(ludus, db)
+                }
+            )
         }
         if (loadDialogueState.value) {
-            LoadLudusDialogue {
-                loadDialogueState.value = it
-            }
+
+            LoadLudusDialogue(
+                databasesList = databases.value,
+                viewModel = viewModel,
+                visibleCallback = { loadDialogueState.value = it },
+                launchLudusManagement = { ludus, db ->
+                    launchLudusManagement(ludus, db)
+                }
+            )
         }
     }
-
-    @OptIn(DelicateCoroutinesApi::class)
-    @Preview
-    @Composable
-    fun NewLudusDialogue(visibleCallback: (Boolean) -> Unit = { newDialogueVisible = it }) {
-        var ludusName by remember { mutableStateOf("") }
-        val databases by remember { mutableStateOf(databases) }
-
-        AlertDialog(
-            onDismissRequest = {
-                visibleCallback(false)
-            },
-            confirmButton = {
-                Button(
-                    onClick = {
-                        Log.d(TAG, "New Ludus: $ludusName")
-                        GlobalScope.launch(Dispatchers.IO) {
-                            val db = createDb(ludusName, applicationContext)
-                            launchLudusManagement(ludusName, db)
-                        }
-                        visibleCallback(false)
-                    },
-                    enabled = ludusName.isNotEmpty() && ludusName !in databases.value.map { it }
-                ) {
-                    Text("Create")
-                }
-            },
-            dismissButton = {
-                Button(onClick = {
-                    visibleCallback(false)
-                    Log.d(TAG, "Cancel button, state: $newDialogueVisible")
-                }) {
-                    Text("Cancel")
-
-                }
-            },
-            title = {
-                Text("Enter the name of your new Ludus:")
-            },
-            text = {
-                OutlinedTextField(
-                    value = ludusName,
-                    onValueChange = { name: String ->
-                        ludusName = name
-                    },
-                    label = { Text("Ludus Name") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-            }
-        )
-    }
-
-    @Preview
-    @Composable
-    fun LoadLudusDialogue(
-        visibleCallback: (Boolean) -> Unit = { loadDialogueVisible = it }
-    ) {
-
-        val databases = remember { databases }
-        val selectedDb = remember { mutableStateOf("") }
-        AlertDialog(
-            modifier = Modifier.fillMaxWidth(),
-            onDismissRequest = { visibleCallback(false) },
-            confirmButton = { },
-            dismissButton = {
-                Button(onClick = { visibleCallback(false) }) {
-                    Text("Dismiss")
-                }
-            },
-            title = { Text("Load Ludus") },
-            text = {
-                LazyColumn {
-                    items(databases.value) { database ->
-                        Column(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .clickable { selectedDb.value = database },
-                            verticalArrangement = Arrangement.Center
-                        ) {
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.SpaceBetween,
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Surface(
-                                    shape = MaterialTheme.shapes.small,
-                                    tonalElevation = 4.dp,
-                                    color = if (selectedDb.value == database) {
-                                        Color.LightGray
-                                    } else {
-                                        Color.Transparent
-                                    }
-                                ) {
-                                    Text(database)
-                                }
-                                Column {
-                                    Row {
-                                        Button(onClick = {
-                                            val db = returnDb(database, applicationContext)
-                                            launchLudusManagement(database, db)
-                                            visibleCallback(false)
-                                        }) { Text("Load") }
-                                        Button(onClick = {
-                                            Log.d(TAG, "deleting: $database")
-                                            deleteDb(database, applicationContext){
-                                                viewModel.setDatabases(getAllDatabases(applicationContext))
-                                            }
-                                        }) { Text("Delete") }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        )
-    }
-
 
     companion object {
         private const val TAG = "StartActivity"
---
+++ app/src/main/java/com/doorxii/ludus/ui/components/NewLudusDialogue.kt
@@ -0,0 +1,76 @@
+package com.doorxii.ludus.ui.components
+
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.doorxii.ludus.data.db.AppDatabase
+import com.doorxii.ludus.utils.DatabaseManagement
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+@OptIn(DelicateCoroutinesApi::class)
+@Composable
+fun NewLudusDialogue(
+    visibleCallback: (Boolean) -> Unit,
+    databases: List<String>,
+    launchLudusManagement: (String, AppDatabase) -> Unit
+) {
+    var ludusName by remember { mutableStateOf("") }
+    val context = LocalContext.current
+    val TAG = "NewLudusDialogue"
+
+    AlertDialog(
+        onDismissRequest = {
+            visibleCallback(false)
+        },
+        confirmButton = {
+            Button(
+                onClick = {
+                    Log.d(TAG, "New Ludus: $ludusName")
+                    GlobalScope.launch(Dispatchers.IO) {
+                        val db = DatabaseManagement.createDb(ludusName, context)
+                        launchLudusManagement(ludusName, db)
+                    }
+                    visibleCallback(false)
+                },
+                enabled = ludusName.isNotEmpty() && ludusName !in databases
+            ) {
+                Text("Create")
+            }
+        },
+        dismissButton = {
+            Button(onClick = {
+                visibleCallback(false)
+                Log.d(TAG, "Cancel button")
+            }) {
+                Text("Cancel")
+
+            }
+        },
+        title = {
+            Text("Enter the name of your new Ludus:")
+        },
+        text = {
+            OutlinedTextField(
+                value = ludusName,
+                onValueChange = { name: String ->
+                    ludusName = name
+                },
+                label = { Text("Ludus Name") },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    )
+}
